dabf55ff3f1868767c01e688e1624a12b0cfbbb6
Chris <christopher.fremond@gmail.com>
2022-08-11 18:07:30 +0100

    adding of a script to launch all tests at once

---
+++ test_all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS='
+'
+# clear the student folder go binary cache
+rm -rf /tmp/binaries/student/
+
+for dir in ./tests/*; do
+    echo "$dir executed"
+    if ! go run "$dir"; then
+        echo "$dir FAILED"
+    fi
+done
